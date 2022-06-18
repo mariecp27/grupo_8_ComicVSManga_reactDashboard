27e29c8db5e25af78df41cd01861369e06dd8eab
mariecp27 <mariaher.cp@gmail.com>
2022-06-18 12:42:56 -0500

    Búsqueda de productos por título

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ChartRow from './ChartRow';
 
 function Chart() {
@@ -7,6 +7,7 @@ function Chart() {
     const [page, setPage] = useState(0);
     const [pagesAmount, setPagesAmount] = useState(0);
     const [productsAmount, setProductsAmount] = useState(0);
+	const [keyword, setKeyword] = useState('');
 
     useEffect(() => {
         fetch(`http://localhost:3030/api/products?page=${page}`)
@@ -19,7 +20,7 @@ function Chart() {
     }, [page]);
 
     useEffect(() => {
-        setPagesAmount(Math.ceil(productsAmount/10));
+        setPagesAmount(Math.ceil(productsAmount / 10));
     }, [productsAmount]);
 
     const previousPage = () => {
@@ -29,10 +30,37 @@ function Chart() {
     }
 
     const nextPage = () => {
-        if(page !== pagesAmount - 1){
+        if (page !== pagesAmount - 1) {
             setPage(page + 1);
         }
     }
+
+	const title = useRef();
+
+    const searchProduct = (e) => {
+		e.preventDefault();
+		setKeyword(title.current.value);
+	}
+
+    useEffect( () => {
+		if(keyword !== ''){
+			fetch(`http://localhost:3030/api/products?page=${page}&search=${keyword}`)
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data.products);
+                setProductsAmount(data.count);
+            })
+            .catch(error => console.error(error));
+		}else{
+            fetch(`http://localhost:3030/api/products?page=${page}`)
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data.products);
+                setProductsAmount(data.count);
+            })
+            .catch(error => console.error(error));
+        }
+    }, [keyword]);
 
     return (
         <div className="card shadow mb-4">
@@ -40,6 +68,13 @@ function Chart() {
                 <h5 className="m-0 font-weight-bold text-gray-800">Listado de Productos</h5>
             </div>
             <div className="card-body">
+                <form method="GET" className='productForm' onSubmit = { searchProduct }>
+                    <label htmlFor="">Buscar por título:</label>
+                    <input type="text" className="form-control" placeholder='Ingresa el nombre del producto' ref = { title }/>
+                    <button className="btn btn-info">
+                        <i className="fas fa-fw fa-search"></i> Buscar
+                    </button>
+                </form>
                 <div className="table-responsive">
                     <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
                         <thead>
@@ -61,19 +96,11 @@ function Chart() {
                             </tr>
                         </tfoot>
                         <tbody>
-                            {products.length === 0 &&
-                                <ChartRow
-                                    id='--'
-                                    name='Cargando'
-                                    description='Cargando'
-                                    categories={['Cargando']}
-                                />
-                            }
                             {
                                 products.map((row, index) => {
                                     return <ChartRow
-                                        { ...row }
-                                        key = { index }
+                                        {...row}
+                                        key={index}
                                     />
                                 })
                             }
@@ -83,8 +110,8 @@ function Chart() {
                 </div>
             </div>
             <div className='pagesButtons'>
-                <button disabled = { page === 0 } onClick= { previousPage }>Anterior</button>
-                <button disabled = { page === pagesAmount - 1 } onClick = { nextPage }>Siguiente</button>
+                <button disabled={page === 0} onClick={previousPage}>Anterior</button>
+                <button disabled={page === pagesAmount - 1} onClick={nextPage}>Siguiente</button>
             </div>
         </div>
 
